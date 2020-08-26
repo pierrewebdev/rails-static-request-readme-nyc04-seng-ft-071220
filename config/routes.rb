e1b3d06d7653256cc558370bc4d72b4fa40962aa
pierrewebdev <patrick.pierre000@gmail.com>
2020-08-25 20:38:15 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'static#about'
+  #this line of code says that when someone requests from the
+  #/about route, ruby should access the static_controller and
+  #check for a method called about
 end
